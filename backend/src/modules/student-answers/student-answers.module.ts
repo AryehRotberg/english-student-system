b7ddbda1a6fbcd90354d290011298e6c5a7511d3
Aryeh Rotberg <rotbergaryeh@gmail.com>
2026-05-04 14:38:49 +0300

    feat: add StudentAnswerRepository to StudentAnswersModule and remove unused InjectRepository import in StudentAnswersService

--- backend/src/modules/student-answers/student-answers.module.ts
+++ backend/src/modules/student-answers/student-answers.module.ts
@@ -4,10 +4,11 @@ import { StudentAnswer } from './entities/student-answer.entity';
 import { StudentAnswersCommon } from './student-answers.common';
 import { StudentAnswersController } from './student-answers.controller';
 import { StudentAnswersService } from './student-answers.service';
+import { StudentAnswerRepository } from './repositories/student-answer.repository';
 
 @Module({
     imports: [TypeOrmModule.forFeature([StudentAnswer])],
     controllers: [StudentAnswersController],
-    providers: [StudentAnswersService, StudentAnswersCommon],
+    providers: [StudentAnswersService, StudentAnswersCommon, StudentAnswerRepository],
 })
 export class StudentAnswersModule {}
